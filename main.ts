08a86a4f30c1c3de1575e2e2dcf6ad1d8392262c
Zoha Nafees <zohanafees09@gmail.com>
2024-06-06 00:19:14 +0500

    TS task 1

---
+++ main.ts
@@ -0,0 +1,4 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let message="Hello Eric, would you like to learn some python today?"
+console.log(message)
